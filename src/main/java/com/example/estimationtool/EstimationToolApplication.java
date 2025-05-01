e25f795c3cb817fef3a6d56adb3923718a1f52e8
Nima <nimaginobilisalami@gmail.com>
2025-05-01 13:42:18 +0200

    Created spring boot project
    added application.properties + dev + prod

---
+++ src/main/java/com/example/estimationtool/EstimationToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.estimationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimationToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstimationToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/estimationtool/EstimationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estimationtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimationToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
