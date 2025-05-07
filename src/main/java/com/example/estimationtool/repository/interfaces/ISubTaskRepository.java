9d99468d8ed36e0600a04f7d8f28dbf75d79ca23
Sofie Broman <sofierytter@hotmail.com>
2025-05-07 11:06:31 +0200

    SubTask-model og ISubTaskRepository interface

---
+++ src/main/java/com/example/estimationtool/repository/interfaces/ISubTaskRepository.java
@@ -0,0 +1,19 @@
+package com.example.estimationtool.repository.interfaces;
+import com.example.estimationtool.model.SubTask;
+import java.util.List;
+
+public interface ISubTaskRepository extends IRepository<SubTask, Integer> {
+
+    SubTask create(SubTask SubTask);
+
+    List<SubTask> readAll();
+
+    SubTask readById(Integer id);
+
+    SubTask update(SubTask subTask);
+
+    void deleteById(Integer id);
+
+}
+
+
