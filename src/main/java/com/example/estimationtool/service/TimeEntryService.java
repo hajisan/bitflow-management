acdec610ac72c4eaac89a9998d43323f9b12154e
Sofie Broman <sofierytter@hotmail.com>
2025-05-08 13:16:30 +0200

    create TimeEntry() i service

--- src/main/java/com/example/estimationtool/service/TimeEntryService.java
+++ src/main/java/com/example/estimationtool/service/TimeEntryService.java
@@ -1,13 +1,25 @@
 package com.example.estimationtool.service;
 
+import com.example.estimationtool.model.timeEntry.TimeEntry;
+import com.example.estimationtool.repository.interfaces.ITimeEntryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TimeEntryService {
 
+    private final ITimeEntryRepository iTimeEntryRepository;
+
+    public TimeEntryService(ITimeEntryRepository iTimeEntryRepository) {
+        this.iTimeEntryRepository = iTimeEntryRepository;
+    }
+
 
     //------------------------------------ Create() ------------------------------------
 
+    public TimeEntry createTimeEntry(TimeEntry timeEntry) {
+
+        return iTimeEntryRepository.create(timeEntry);
+    }
 
     //------------------------------------ Read() --------------------------------------
 
