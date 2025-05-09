bf596e5964f90da082e620f9794307b601503499
Joakim R. S. <jose0001@stud.kea.dk>
2025-05-09 14:59:09 +0200

    Jeg har tilføjet samme inputvalidering til update som i create og gjort så Deadline er required i HTML-formen (ellers bliver den ikke sat og vi får en exception

--- src/main/java/com/example/estimationtool/service/SubProjectService.java
+++ src/main/java/com/example/estimationtool/service/SubProjectService.java
@@ -76,6 +76,21 @@ public class SubProjectService {
     //------------------------------------ Update() ------------------------------------
     public SubProject update(UserViewDTO currentUser, SubProject subProject) {
         RoleCheck.ensureAdminOrProjectManager(currentUser.getRole());
+
+        // Inputvalidering til update
+        if (subProject.getName() == null || subProject.getName().isBlank()) {
+            throw new IllegalArgumentException("Subprojektets navn må ikke være tomt.");
+        }
+        if (subProject.getEstimatedTime() <= 0) {
+            throw new IllegalArgumentException("Subprojektets estimerede tid må ikke være 0 eller negativ.");
+        }
+        if (subProject.getDeadline() == null) {
+            throw new IllegalArgumentException("Subprojektets deadline må ikke være null eller tom.");
+        }
+        if (subProject.getStatus() != Status.ACTIVE && subProject.getStatus() != Status.INACTIVE && subProject.getStatus() != Status.DONE) {
+            throw new IllegalArgumentException("Subprojektets status skal være sat til enten Active, Inactive eller Done.");
+        }
+
         return iSubProjectRepository.update(subProject);
     }
 
