a72304927ce1f2a6bcb1710f79fe310b5caf83e0
Nima <nimaginobilisalami@gmail.com>
2025-05-15 21:38:15 +0200

    UserFriendlyException metoden indeholder nu,
    også String til redirectPath, så når den kaldes, definere man fejlbeskeden og hvor brugeren skal redirectes til, for at forbedre UX

--- src/main/java/com/example/estimationtool/toolbox/exceptionHandler/UserFriendlyException.java
+++ src/main/java/com/example/estimationtool/toolbox/exceptionHandler/UserFriendlyException.java
@@ -1,15 +1,21 @@
 package com.example.estimationtool.toolbox.exceptionHandler;
 
-import javax.management.RuntimeErrorException;
-
 /*
-Skriv noget om hvad denne klasse gør og bruges til
+ * Denne klasse repræsenterer en specialdesignet exception, der bruges til at give brugervenlige fejlbeskeder.
+ * Den håndterer fejl på en måde, der undgår at vise tekniske detaljer, som f.eks. stack traces, i frontend.
+ * Exceptions inkluderer en fejlbesked til brugeren og en redirect path, der guider dem til den rette side efter fejlen.
  */
 
 public class UserFriendlyException extends RuntimeException {
+    private final String redirectPath;
 
-    public UserFriendlyException(String message) {
+    public UserFriendlyException(String message, String redirectPath) {
         super(message);
+        this.redirectPath = redirectPath;
+    }
+
+    public String getRedirectPath() {
+        return redirectPath;
     }
 
 }
